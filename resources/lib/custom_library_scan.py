2c66a428bb0b58b09af6bf259e1f8f776ee2e30e
bossanova808 <bossanova808@gmail.com>
2020-09-02 16:44:27 +1000

    Very minor progres...

--- resources/lib/custom_library_scan.py
+++ resources/lib/custom_library_scan.py
@@ -2,11 +2,26 @@
 
 import xbmc
 import time
+import json
 from resources.lib.common import *
+
 
 def run():
 
     footprints()
+
+    command = json.dumps({
+        'jsonrpc': '2.0',
+        'id': 0,
+        'method': 'VideoLibrary.Scan',
+        'params': {
+            'directory': 'C:/XBMC/Video Files/TV01/',
+            'showdialogs': True
+        }
+    })
+
+    send_kodi_json("Kick off video library scan", command)
+
 
     # # Run until abort requested
     # while not kodi_monitor.abortRequested():
@@ -16,3 +31,4 @@ def run():
     #         break
 
     footprints(False)
+
